07e780f2e52d5f5c0efc71cb572bec07393a4846
Ajin <ajinfg03@gmail.com>
2026-03-10 12:46:40 +0530

    New Folder Create and update

---
+++ app.js
@@ -0,0 +1,33 @@
+//remove the first element
+let numbers1 = [1,2,3,4,5];
+
+numbers1.shift();
+
+console.log(numbers1);
+
+//Add last element array
+let numbers2 = [1,2,3,4,5];
+
+numbers2.push();
+
+console.log(numbers2);
+
+//Remove last element 
+let numbers3 = [1,2,3,4,5];
+
+numbers3.pop();
+
+console.log(numbers3);
+
+//Add value start of Array
+let numbers4 = [1,2,3,4,5];
+
+numbers4.unshift(0);
+
+console.log(numbers4);
+
+//Return the number of the elements
+let fruties =  ["Apple","Orange","Pineapple"];
+console.log(fruties.length);
+
+//Array Map(Transform Array)
